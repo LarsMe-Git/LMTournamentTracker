002e881a94481e878f5b868ce3a35136f1824557
Lars Meske <lars.meske@stud.uni-due.de>
2019-07-08 22:24:08 +0200

    Lesson 9 completed

--- TrackerLibrary/Models/MatchupEntryModel.cs
+++ TrackerLibrary/Models/MatchupEntryModel.cs
@@ -9,7 +9,7 @@ namespace TrackerLibrary.Models
     public class MatchupEntryModel
     {
         /// <summary>
-        /// Represents one Team in the Matchup testtestzerr
+        /// Represents one Team in the Matchup git läuft
         /// </summary>
         public TeamModel Teamcompeting { get; set; }
         /// <summary>
